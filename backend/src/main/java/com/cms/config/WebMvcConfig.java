3a1cbdf69d252c14e0182ef08bb94047b41c6d0f
TheNoobBot <49305197+TheNoobBot@users.noreply.github.com>
2020-05-03 13:50:43 +0300

    Register and login flow implemented with some basic logging. (#9)

--- backend/src/main/java/com/cms/config/WebMvcConfig.java
+++ backend/src/main/java/com/cms/config/WebMvcConfig.java
@@ -16,7 +16,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loggingInterceptor);
-        registry.addInterceptor(requestInterceptor).excludePathPatterns("/login", "/register", "/conferences");
+        registry.addInterceptor(requestInterceptor).excludePathPatterns("/login", "/register");
         super.addInterceptors(registry);
     }
 }
