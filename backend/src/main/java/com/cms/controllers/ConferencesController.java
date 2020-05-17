f4cd623afc67d18415c002d07c4526acf23288e6
Mihai Socaciu <mihai.socaciu@tss-yonder.com>
2020-05-17 15:07:41 +0300

    Cors and exclude /conferences.

--- backend/src/main/java/com/cms/controllers/ConferencesController.java
+++ backend/src/main/java/com/cms/controllers/ConferencesController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/conferences")
 public class ConferencesController {
